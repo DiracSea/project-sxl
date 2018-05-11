ca5327f13322b8b4917cfa2a0f950856443af0e0
DiracSea <sulz@mail.sustc.edu.cn>
2018-05-11 15:41:24 +0800

    data_pros_2

--- lr/data_pros.py
+++ lr/data_pros.py
@@ -1,6 +1,15 @@
+from db import *
+from minibatch-sgd import *
 
 def read_data():
-    pass
+    rain = conn_db('rainfall','rainfall','',0,'','')
+    device = conn_db('clone_device','xxx','',0,'','')
+    for row in rain.export():
+        
+        yield row
+    for row in device.export():
+        
+        pass
 
 def slice_data():
-    pass
+    return X,y,Xt,yt
